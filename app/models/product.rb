72837f1dcf829934871dd9be8cb8850e10caad1e
Ankit Singh <ankitas90@gmail.com>
2019-08-12 21:46:34 +0530

    added- callbacks, set sku_code and wh_code in models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ApplicationRecord
 	belongs_to :warehouse
 
-	validates_presence_of :name, :price, :sku_code
+	before_create :set_sku_code
+
+	validates_presence_of :name, :price
+
+	private
+
+	def set_sku_code
+		size = 5
+		charset = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+		code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+		self.sku_code = 'PR-' + code 
+	end
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,19 @@
 class Warehouse < ApplicationRecord
+	
 	has_many :products, dependent: :destroy
 	
-	validates_presence_of :name, :wh_code
+	before_create :set_wh_code
+	
+	validates_presence_of :name, :pincode
+	validates :wh_code, uniqueness: true
+
+	private
+
+	def set_wh_code
+  	code_size = (4..16).to_a
+  	charset = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+  	code = (0...code_size[rand(code_size.size)]).map{ charset.to_a[rand(charset.size)] }.join
+  	self.wh_code = code
+	end
+
 end
