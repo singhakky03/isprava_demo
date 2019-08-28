4d2dc76fc2fe2ccdfcd33e8f2fd6ac729348cbbf
Ankit Singh <ankitas90@gmail.com>
2019-08-29 00:57:43 +0530

    added view of product index as root url and new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   end
 
   def index
+  	@products = Product.all
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'warehouses/new'
-  get 'warehouses/index'
-  get 'warehouses/edit'
-  get 'products/new'
-  get 'products/index'
-  get 'products/edit'
+  root to: "products#index"
+  resources :warehouses
+  resources :products
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
